dfa92a8e0694725c33017defb0f1ef3a06812275
John Arnold <photowalkthrough@gmail.com>
2024-12-13 20:59:57 +0000

    Added an algebraic solution courtesy of Phaul

--- day13/d13p2.py
+++ day13/d13p2.py
@@ -6,13 +6,13 @@ input = open("day13/input.txt", "r").read().split("\n")
 # Credit to Eric Veenendaal for this solution. He put me onto learning
 # about sympy. I understand this solution now but I didn't come up with
 # it. 
-def solve_with_sympy(x1, y1, x2, y2, goal_x, goal_y):
+def solve_with_sympy(ax, ay, bx, by, px, py):
     # Define the symbols
     a, b = solver.symbols('a b')
 
     # Define the equations
-    eq1 = solver.Eq(x1 * a + x2 * b, goal_x)
-    eq2 = solver.Eq(y1 * a + y2 * b, goal_y)
+    eq1 = solver.Eq(ax * a + bx * b, px)
+    eq2 = solver.Eq(ay * a + by * b, py)
 
     # Solve the system of equations and produce a list of solution values
     solution = solver.solve((eq1, eq2), (a, b))
@@ -22,6 +22,18 @@ def solve_with_sympy(x1, y1, x2, y2, goal_x, goal_y):
         return(int(solution[a]), int(solution[b]))
 
     return 0,0
+
+# Credit to Phaul for this solution which is algebraic. He solved for the
+# two equations and you get these neat equations for a and b. The numbers just
+# drop out. The only extra work is to ensure that you only return the solutions
+# that are whole numbers. Those that give decimals are the unsolvable machines.
+def solve_with_algebra(ax, ay, bx, by, px, py):
+    b = (py*ax - px*ay) / (by*ax - bx*ay)
+    a = (py - by*b)/ay
+    if a.is_integer() and b.is_integer():
+        return(int(a),int(b))
+    else:
+        return(0,0)
 
 # Parse the input into machines list. Each machine is a dict.
 machine = dict()
@@ -42,7 +54,14 @@ machines.append(machine)
 
 total = 0
 for m in machines:
+    (a, b) = solve_with_algebra(m['Ax'], m['Ay'], m['Bx'],m['By'],m['Px'],m['Py'])
+    total += a*3 + b
+
+print(f"Total with algebra: {total}")
+
+total = 0
+for m in machines:
     (a, b) = solve_with_sympy(m['Ax'], m['Ay'], m['Bx'],m['By'],m['Px'],m['Py'])
     total += a*3 + b
 
-print(f"Total: {total}")
+print(f"Total with sympy: {total}")
